d241b64f171a6f26331d5c64dedb1c040d1b7026
Dyllan Castia <castiadyllan@gmail.com>
2026-02-02 10:08:46 -0800

    Create Question1.cpp

---
+++ Lab1/Question1.cpp
@@ -0,0 +1,21 @@
+// This program prints to the screen the words: 
+// PI = 3.14 
+// Radius = 4 
+// Circumference = 25.12 
+#include <iostream> 
+using namespace std; 
+const double PI = 3.14; 
+int main() 
+{ 
+float radius; 
+radius = 4.0; 
+cout << "PI = " << PI << endl; 
+cout << "Radius = " << radius << endl; 
+cout << "Circumference = " << 2 * PI * radius << endl; 
+return 0; 
+} 
+/*program output
+PI = 3.14
+Radius = 4
+Circumference = 25.12
+*/
