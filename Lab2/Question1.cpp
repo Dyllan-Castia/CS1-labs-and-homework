f60334542b1ceb95546aaaf1822cce3499677cfe
Dyllan Castia <castiadyllan@gmail.com>
2026-02-02 10:14:42 -0800

    Create Question1.cpp

---
+++ Lab2/Question1.cpp
@@ -0,0 +1,16 @@
+/*Dyllan Castia. 1/28/26
+Lab 2, Question 1: Calculate the sum of 50 and 100 and store
+the result in a variable called total. Print the output of total.
+*/
+#include <iostream>
+using namespace std;
+const int num1 = 50;
+const int num2 = 100;
+const int total = num1 + num2;
+
+int main()
+{
+    cout << "The sum of 50 and 100 is: " << total << endl;
+    return 0;
+}
+//The sum of 50 and 100 is: 150
