e12af91f4ab63e8a6f35c0e295b45a6db85d1d5f
Dyllan Castia <castiadyllan@gmail.com>
2026-02-02 10:12:04 -0800

    Create Question2.cpp

---
+++ Lab1/Question2.cpp
@@ -0,0 +1,19 @@
+#include <iostream> 
+using namespace std; 
+const double PI = 3.14; 
+int main() 
+{ 
+float radius; 
+radius = 4.0; 
+cout << "Dyllan Castia" << endl; 
+cout << "*** Ta***er Rd"<< endl; 
+cout << "L*******e, CA 9***1"<< endl; 
+cout << "(***)-***-****"<< endl;
+return 0; 
+} 
+/*Output:
+Dyllan Castia
+*** Ta***er Rd
+L*******e, CA 9***1
+(***)-***-****
+*/
