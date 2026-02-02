51b238111de8343ac6bd8ef374d4395d0038fb64
Dyllan Castia <castiadyllan@gmail.com>
2026-02-02 09:53:50 -0800

    Create hwCh2Question8.cpp

---
+++ hwCh2Question8.cpp
@@ -0,0 +1,24 @@
+/*
+Dyllan Castia
+01/30/26
+Triangle Pattern:
+This program prints a triangle pattern using asterisks.
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Print the triangle pattern
+    cout << "   *   " << endl;
+    cout << "  ***  " << endl;
+    cout << " ***** " << endl;
+    cout << "*******" << endl;
+    return 0;
+}
+/*
+Output:
+   *   
+  ***
+ *****
+*******
+*/
